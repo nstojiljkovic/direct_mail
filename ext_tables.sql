d3fa2c5ae53168ef160ddaf4f2cfcfe09e9cb41c
Nikola Stojiljkovic <nikola.stojiljkovic@essentialdots.com>
2014-05-16 21:04:27 +0200

    [BUGFIX] Additional TYPO3 6.2 compatibility patches

--- ext_tables.sql
+++ ext_tables.sql
@@ -164,6 +164,16 @@ CREATE TABLE sys_dmail_category (
     category tinytext NOT NULL,
     old_cat_number char(2) DEFAULT '' NOT NULL,
 
+    t3ver_oid int(11) DEFAULT '0' NOT NULL,
+    t3ver_id int(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+    t3ver_label varchar(30) DEFAULT '' NOT NULL,
+    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_count int(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+    t3_origuid int(11) DEFAULT '0' NOT NULL,
+
     PRIMARY KEY (uid),
     KEY parent (pid)
 );
